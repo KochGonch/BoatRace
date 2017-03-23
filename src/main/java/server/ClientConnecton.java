f2e49921216eaa59c4c58f4729f6c080c7f40f55
kochetovadasha <ko4.dasha@yandex.ru>
2017-03-23 23:59:29 +0300

    good connected

--- src/main/java/server/ClientConnecton.java
+++ src/main/java/server/ClientConnecton.java
@@ -1,12 +1,24 @@
 package server;
 
+import client.Coord;
+
 import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * Created by Даша on 16.03.2017.
  */
 public class ClientConnecton extends Thread {
     private Socket socket;
+    private Coord clientCoord;
+    private Coord newCoord;
+    int clientX;
+    int clientY;
 
     public ClientConnecton(Socket socket) {
         this.socket = socket;
@@ -14,6 +26,18 @@ public class ClientConnecton extends Thread {
 
     @Override
     public void run() {
-        System.out.println("ekhke");
+        try {
+            ObjectInputStream inFromClient = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
+            clientCoord = new Coord((Coord)inFromClient.readObject());
+            clientX = clientCoord.getX();
+            clientY = clientCoord.getY();
+            newCoord = new Coord(clientX + 2, clientY + 2);
+            outToClient.writeObject(newCoord);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/server/RBServer.java
+++ src/main/java/server/RBServer.java
@@ -6,17 +6,15 @@ import java.net.*;
 public class RBServer {
     public static void main(String[] args){
         try {
-            ServerSocket welcomeSocket = new ServerSocket(12345);
+            ServerSocket welcomeSocket = new ServerSocket(56789);
             while(true) {
                 Socket connectionSocket = welcomeSocket.accept();
+                System.out.println("Hi, client");
                 new ClientConnecton(connectionSocket).start();
                 System.out.println("Client connected");
-
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
