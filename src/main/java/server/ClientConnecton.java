b67ea9c81d33c6f5ad4735c9427cee21cd4d69c1
kochetovadasha <ko4.dasha@yandex.ru>
2017-05-14 19:39:30 +0300

    i am go out

--- src/main/java/server/ClientConnecton.java
+++ src/main/java/server/ClientConnecton.java
@@ -22,12 +22,43 @@ public class ClientConnecton extends Thread {
     private Coord newCoord;
     int clientX;
     int clientY;
+    public boolean [][] pos = new boolean [20][20]; // это типо битовое поле как у Бори
 
     public ClientConnecton(Socket socket) {
-        this.socket = socket;
+        this.socket = socket;  // инициализация сокета
+        for (int i=0; i<20; i++)  // создание поля
+            for (int j=0; j<20; j++) pos[i][j]=false;
+        for (int i=1; i<20; i+=4) // созание препятствий
+            for (int j=1; j<20; j+=4) {
+                pos[i][j]=true;
+            }
     }
 
-    public boolean ChangeDir (Coord coord1)
+    synchronized void sendHelm(ClientConnecton connection)  // это то что я при тебе перепечатывала у Бори
+    {                                                      //провека свободна ли клетка и перемещение если свободна
+        if ((clientCoord.getTX()-clientCoord.getX()>0)&&(!pos[clientCoord.getX()+1][clientCoord.getY()])) {
+            clientCoord.Setdir(Direction.RIGHT);
+            pos[clientCoord.getX()+1][clientCoord.getY()]=true;
+            pos[clientCoord.getX()][clientCoord.getY()]=false;
+        }
+        else if ((clientCoord.getTX()-clientCoord.getX()<0)&&(!pos[clientCoord.getX()-1][clientCoord.getY()])) {
+            clientCoord.Setdir(Direction.LEFT);
+            pos[clientCoord.getX()-1][clientCoord.getY()]=true;
+            pos[clientCoord.getX()][clientCoord.getY()]=false;
+        }
+        else if ((clientCoord.getTY()-clientCoord.getY()>0)&&(!pos[clientCoord.getX()][clientCoord.getY()+1])) {
+            clientCoord.Setdir(Direction.GO);
+            pos[clientCoord.getX()][clientCoord.getY()+1]=true;
+            pos[clientCoord.getX()][clientCoord.getY()]=false;
+        }
+        else if ((clientCoord.getTY()-clientCoord.getY()<0)&&(!pos[clientCoord.getX()][clientCoord.getY()-1])) {
+            clientCoord.Setdir(Direction.BACK);
+            pos[clientCoord.getX()][clientCoord.getY()-1]=true;
+            pos[clientCoord.getX()][clientCoord.getY()]=false;
+        }
+    }
+
+    public boolean ChangeDir (Coord coord1)  // это тоже проверка нет ли рядом других лодок но которую я писала до битового поля
     {
         int x = 0;
         int y = 0;
@@ -64,19 +95,24 @@ public class ClientConnecton extends Thread {
         return  false;
     }
 
-
+    public void removeClient(ClientConnecton connecton) {  //это вот как раз удаление клиента, хз где это лучше делать
+        pos[clientCoord.getX()][clientCoord.getY()]=false;
+        clients.remove(connecton);
+    }
 
     @Override
     public void run() {
         try {
-            ObjectInputStream inFromClient = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream inFromClient = new ObjectInputStream(socket.getInputStream()); //переменная отвечающая за прием объекта
+            ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream()); //за передачу объекта
             while(true) {
-                clientCoord = new Coord((Coord) inFromClient.readObject());
-                clientX = clientCoord.getX();
+                clientCoord = new Coord((Coord) inFromClient.readObject()); //присваиваем клиентКоорд полученный от клиента объект
+                clientX = clientCoord.getX();  // это на всякий случай, чисто для удобства
                 clientY = clientCoord.getY();
-                newCoord = new Coord(clientX + 2, clientY + 2, Direction.LEFT);
-                outToClient.writeObject(newCoord);
+                sendHelm(this);
+                //newCoord = new Coord(clientX + 2, clientY + 2, Direction.LEFT); // тестовое изменение координа
+                                              // вообще тут должны быть проверки все и высчитывание новых координат и направления
+                outToClient.writeObject(newCoord); // отправляем новый объект клиенту
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -84,6 +120,4 @@ public class ClientConnecton extends Thread {
             e.printStackTrace();
         }
     }
-
-
 }
--- src/main/java/server/RBServer.java
+++ src/main/java/server/RBServer.java
@@ -1,24 +1,30 @@
 package server;
 
+import client.Coord;
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RBServer {
-    public static List<ClientConnecton> clients = new ArrayList<ClientConnecton>();
+    public static List<ClientConnecton> clients = new ArrayList<ClientConnecton>(); // это список подключений клиента,
+                                                                                    // когда подключается новый добавляяем сюда,
+                                                                            //когда он проиграл или выйграл или отключился удаляем
 
     public static void main(String[] args){
         try {
-            ServerSocket welcomeSocket = new ServerSocket(56789);
+            ServerSocket welcomeSocket = new ServerSocket(56789);   // это порт по которому соединяются с сервером
             while(true) {
-                Socket connectionSocket = welcomeSocket.accept();
-                System.out.println("Hi, client");
-                ClientConnecton connecton = new ClientConnecton(connectionSocket);
-                clients.add(connecton);
-                connecton.start();
-                System.out.println("Client connected");
-                clients.remove(connecton); // когда проиграл
+                Socket connectionSocket = welcomeSocket.accept();    // асепт - это мы бесконечно ждем клиентов
+                System.out.println("Hi, client");   // это просто вывод ради проверки
+                ClientConnecton connecton = new ClientConnecton(connectionSocket); // когда подключился клиент создается класс
+                                                                                    // который поток для его обработки
+                clients.add(connecton); // это мы добавляем в список клиентов новое соединение
+                connecton.start();  // запускаем поток обработки клиента
+                System.out.println("Client connected");  // тоже ради прикола, можно удалить
+                clients.remove(connecton); // когда проиграл, удаляем из списка, но там появилась функци удаления...
+                                                // надо придумать как лучше это делать
             }
         } catch (IOException e) {
             e.printStackTrace();
